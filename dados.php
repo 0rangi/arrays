29011a40e1ad8026880272d2328b5390397a2307
0rangi <hadassamoreno529@gmail.com>
2024-02-27 17:34:13 -0300

    aaaaaaaaaaaaaaaa

---
+++ dados.php
@@ -0,0 +1,8 @@
+<?php
+$var[]= array("nome" => "belinha", "idade" => 4, "tipo" => "cachorro");
+$var[]= array("nome" => "Arlindo", "idade" => 2, "tipo" => "gato");
+$var[]= array("nome" => "mimi", "idade" => 4, "tipo" => "gato");
+$var[]= array("nome" => "rex", "idade" => 7, "tipo" => "cachorro");
+$var[]= array("nome" => "mel", "idade" => 1, "tipo" => "cachorro");
+$var[]= array("nome" => "cappuccino", "idade" => 5, "tipo" => "periquito");
+?>
---
+++ todos.php
@@ -0,0 +1,22 @@
+<?php 
+include 'dados.php';
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+
+    <link href ="bootstrap/css/bootstrap.min.css" rel="stylesheet">
+    <link href="jumbotron.css" rel="stylesheet">
+</head>
+<body>
+   <div>
+      <div class="container">
+
+       </div>
+   </div> 
+</body>
+</html>
